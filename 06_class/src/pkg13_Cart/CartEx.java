1ca401b55d5212cf8fc67c66eb20129f63e0a3a0
oeoroeor123 <oeoroeor123@naver.com>
2024-11-08 18:01:50 +0900

    20241108

--- 06_class/src/pkg13_Cart/CartEx.java
+++ 06_class/src/pkg13_Cart/CartEx.java
@@ -1,27 +1,53 @@
 package pkg13_Cart;
 
-import java.util.Arrays;
-
 public class CartEx {
 
   public static void main(String[] args) {
+
+    // 10000원을 가진 고객
+    Customer customer = new Customer(10000);
     
-    Cart cart = new Cart(5); // cart에 담을 물건 갯 수
+    // 5개 물건 담을 수 있는 cart 생성
+    Cart cart = new Cart(5);
+    
+    // 고객이 cart 를 가짐
+    customer.setCart(cart);
+    
+    // 물건 넣기 
+    customer.addToCart(new Product("맛동산", 3000));
+    customer.addToCart(new Product("양파링", 2000));
+    customer.addToCart(new Product("홈런볼", 5000));
+    customer.addToCart(new Product("스윙칩", 1500));
+    
+    // 물건 빼기
+    customer.removeFromCart(0);
+    
+    // 구매하면 영수증이 반환됨
+    String receipt = customer.buy();
+    
+    // 영수증 확인
+    System.out.println(receipt);
+    
+    // 고객 정보 확인
+    System.out.println("고객이 가진 돈 : " + customer.getMoney() + "원");
+    
+    /*
+    Cart cart = new Cart(5);
     
     cart.addProduct(new Product("새우깡", 1000));
-    cart.addProduct(new Product("tv", 10000));
-    cart.addProduct(new Product("건조기", 10000));
-    cart.addProduct(new Product("침대", 30000));
-    cart.addProduct(new Product("드라이기", 5000));
+    cart.addProduct(new Product("TV", 2000));
+    cart.addProduct(new Product("컵", 3000));
+    cart.addProduct(new Product("낙지", 4000));
+    cart.addProduct(new Product("카메라", 5000));
     
-    // 삭제 (모두 0인 이유 : 첫번째 물건이 삭제되면 두번째 물건이 1번으로 오기 때문)
     cart.removeProduct(0);
     cart.removeProduct(0);
     
     for(int i = 0; i < cart.getIdx(); i++) {
       System.out.println(cart.getProducts()[i].getName());
       System.out.println("  " + cart.getProducts()[i].getPrice());
-    }   
+    }
+    */
     
   }
 
